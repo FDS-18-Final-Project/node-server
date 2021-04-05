5b995bca687f11058e395c2440650dbac234f0d6
dannylee9212 <dannylee9212@gmail.com>
2021-04-05 13:23:07 +0900

    feat: webpack builder added

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  // entry file
+  // https://webpack.js.org/configuration/entry-context/#entry
+  entry: ['@babel/polyfill', './src/server.js'],
+  // 번들링된 js 파일의 이름(filename)과 저장될 경로(path)를 지정
+  // https://webpack.js.org/configuration/output/#outputpath
+  // https://webpack.js.org/configuration/output/#outputfilename
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: 'bundle.js'
+  },
+  target: 'node',
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false, // if you don't put this is, __dirname
+    __filename: false // and __filename return blank or /
+  },
+  // https://webpack.js.org/configuration/module
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src')],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  // https://webpack.js.org/configuration/mode
+  mode: 'development'
+};
