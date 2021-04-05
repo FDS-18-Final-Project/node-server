64bbb595e6288f199d3bbc62993aa6896d35cc25
dannylee9212 <dannylee9212@gmail.com>
2021-04-05 13:27:10 +0900

    fix: remove cor

--- src/server.js
+++ src/server.js
@@ -11,7 +11,6 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./'));
-app.use(cors());
 app.use(express.json());
 
 // db
